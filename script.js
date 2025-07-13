80486e41eb7aa83b60545a98c6b883bbe3a20382
michmart1208 <michi.pino2004@gmail.com>
2025-07-13 19:09:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,135 +1,162 @@
-// Definición de ramos con semestres, créditos y dependencias
-const ramos = {
-  "Matemáticas I": {sem:1,cr:4, deps: [], opens:["Física medica"]},
-  "Química general": {sem:1,cr:4, deps: [], opens:["Química orgánica"]},
-  "Biología I": {sem:1,cr:2, deps: [], opens:["Biología II"]},
-  "Comprensión y Expresión Lingüística I": {sem:1,cr:3, deps: []},
-  "Desarrollo de destrezas para el aprendizaje": {sem:1,cr:2, deps: []},
-  "Laboratorio de Biología I": {sem:1,cr:1, deps: [], opens:["Laboratorio de Biología II"]},
-  "Extra Académica Cultural o Deportiva": {sem:1,cr:1, deps: []},
-  "Sociología de la Salud": {sem:2,cr:2, deps: [], opens:["Ciencias Sociales","ITPP I"]},
-  "Química orgánica": {sem:2,cr:3, deps:["Química general"]},
-  "Estadística general": {sem:2,cr:3, deps: []},
-  "Inglés Instrumental": {sem:2,cr:3, deps: []},
-  "Biología II": {sem:2,cr:3, deps:["Biología I"]},
-  "Laboratorio de Biología II": {sem:2,cr:1, deps:["Laboratorio de Biología I"]},
-  "Física medica": {sem:2,cr:3, deps:["Matemáticas I"]},
-  "Anatomía I": {sem:3,cr:7, deps:["Biología II","Laboratorio de Biología II"], opens:["Anatomía II","ITPP II"]},
-  "Embriología": {sem:3,cr:3, deps:["Biología II","Laboratorio de Biología II"], opens:["Anatomía II","Psicología evolutiva"]},
-  "Ciencias Sociales": {sem:3,cr:2, deps:["Sociología de la Salud"]},
-  "ITPP I": {sem:3,cr:1, deps:["Sociología de la Salud"], opens:["ITPP II"]},
-  "Bioquímica": {sem:4,cr:6, deps:[], opens:["Fisiología"]},
-  "Anatomía II": {sem:4,cr:3, deps:["Anatomía I","Embriología"], opens:["Fisiología","Histología"]},
-  "Psicología evolutiva": {sem:4,cr:5, deps:["Embriología"], opens:["Psicología medica"]},
-  "Informática": {sem:4,cr:1, deps:[], opens:["Estadística general"]},
-  "ITPP II": {sem:4,cr:1, deps:["ITPP I"], opens:["ITPP III"]},
-  "Fisiología": {sem:5,cr:8, deps:["Bioquímica","Anatomía II"], opens:["Microbiología e inmunología clínica","Fisiopatología","Parasitología","Psicología medica"]},
-  "Histología": {sem:5,cr:4, deps:["Anatomía II"], opens:["Microbiología e inmunología clínica","Fisiopatología","Parasitología"]},
-  "Estadística": {sem:5,cr:2, deps:["Informática","Estadística general"], opens:["Epidemiologia general y saneamiento ambiental"]},
-  "ITPP III": {sem:5,cr:1, deps:["ITPP II"], opens:["ITPP IV"]},
-  "Electiva": {sem:5,cr:1, deps:[]},
-  "Microbiología e inmunología clínica": {sem:6,cr:5, deps:["Fisiología","Histología"], opens:["Medicina I","Anatomía patológica"]},
-  "Parasitología": {sem:6,cr:4, deps:["Fisiología","Histología"], opens:["Medicina I","Anatomía patológica"]},
-  "Epidemiologia general y saneamiento ambiental": {sem:6,cr:3, deps:["Estadística"], opens:["Epidemiologia especial"]},
-  "Psicología medica": {sem:6,cr:6, deps:["Fisiología","Psicología evolutiva"], opens:["Puericultura","Psicopatología"]},
-  "ITPP IV": {sem:6,cr:1, deps:["ITPP III"]},
-  "Medicina I": {sem:7,cr:6, deps:["Microbiología e inmunología clínica","Parasitología"], opens:["Cirugía I","Ginecología y Obstetricia I","Puericultura","Psicopatología","Medicina II","Genética"]},
-  "Fisiopatología": {sem:7,cr:4, deps:["Fisiología"], opens:["Farmacología I","Medicina II"]},
-  "Anatomía patológica": {sem:7,cr:6, deps:["Microbiología e inmunología clínica","Parasitología"], opens:["Cirugía I","Ginecología y Obstetricia I"]},
-  "Cirugía I": {sem:8,cr:6, deps:["Medicina I","Anatomía patológica"], opens:["Cirugía II"]},
-  "Ginecología y Obstetricia I": {sem:8,cr:3, deps:["Medicina I","Anatomía patológica"], opens:["Ginecología y Obstetricia II"]},
-  "Puericultura": {sem:8,cr:2, deps:["Psicología medica","Medicina I"], opens:["Pediatría I"]},
-  "Farmacología I": {sem:8,cr:4, deps:["Fisiopatología"], opens:["Farmacología II"]},
-  "Genética": {sem:8,cr:1, deps:["Medicina I"]},
-  "Pediatría I": {sem:9,cr:6, deps:["Puericultura"], opens:["Pediatría II"]},
-  "Medicina II": {sem:9,cr:5, deps:["Medicina I","Fisiopatología"], opens:["Medicina III","Imagenología"]},
-  "Farmacología II": {sem:9,cr:2, deps:["Farmacología I"], opens:["Medicina III"]},
-  "Psicopatología": {sem:9,cr:2, deps:["Psicología medica","Medicina I"], opens:["Psiquiatría clínica"]},
-  "Electiva 9": {sem:9,cr:1, deps:[]},
-  "Medicina III": {sem:10,cr:4, deps:["Medicina II","Farmacología II"], opens:["Medicina IV"]},
-  "Cirugía II": {sem:10,cr:3, deps:["Cirugía I"], opens:["Medicina legal","Cirugía III"]},
-  "Ginecología y Obstetricia II": {sem:10,cr:6, deps:["Ginecología y Obstetricia I"], opens:["Medicina legal"]},
-  "Deontología medica": {sem:10,cr:1, deps:[]},
-  "Epidemiologia especial": {sem:10,cr:2, deps:["Epidemiologia general y saneamiento ambiental"], opens:["Administración medica"]},
-  "Medicina IV": {sem:11,cr:5, deps:["Medicina III"], opens:["Medicina V"]},
-  "Pediatría II": {sem:11,cr:6, deps:["Pediatría I"], opens:["Pediatría III"]},
-  "Imagenología": {sem:11,cr:1, deps:["Medicina II"], opens:["Cirugía III"]},
-  "Administración medica": {sem:11,cr:2, deps:["Epidemiologia especial"], opens:["Medicina del trabajo"]},
-  "Medicina legal": {sem:11,cr:1, deps:["Cirugía II","Ginecología y Obstetricia II"]},
-  "Psiquiatría clínica": {sem:12,cr:5, deps:["Psicopatología"], opens:["Higiene mental y psicoterapia"]},
-  "Medicina V": {sem:12,cr:6, deps:["Medicina IV"], opens:["Medicina VI"]},
-  "Cirugía III": {sem:12,cr:3, deps:["Cirugía II","Imagenología"], opens:["Cirugía IV"]},
-  "Medicina del trabajo": {sem:12,cr:1, deps:["Administración medica"], opens:["Pasantía rural"]},
-  "Historia de la medicina": {sem:12,cr:1, deps:[]},
-  "Medicina VI": {sem:13,cr:4, deps:["Medicina V"]},
-  "Pediatría III": {sem:13,cr:6, deps:["Pediatría II"]},
-  "Higiene mental y psicoterapia": {sem:13,cr:1, deps:["Psiquiatría clínica"]},
-  "Trabajo de grado": {sem:13,cr:4, deps:[]},
-  "Cirugía IV": {sem:13,cr:4, deps:["Cirugía III"]},
-  "Ginecología y Obstetricia III": {sem:13,cr:4, deps:["Ginecología y Obstetricia II"]},
-  "Pasantía rural": {sem:13,cr:4, deps:["Medicina del trabajo"]}
-};
+// 1) Define todos los cursos con semestre y prerrequisitos
+const courses = [
+  { id:'MAT1', name:'Matemáticas I', credits:4, sem:1, prereqs:[] },
+  { id:'QG',   name:'Química general', credits:4, sem:1, prereqs:[] },
+  { id:'BIO1', name:'Biología I', credits:2, sem:1, prereqs:[] },
+  { id:'CEL1', name:'Comprensión y Expresión Lingüística I', credits:3, sem:1, prereqs:[] },
+  { id:'DDA',  name:'Desarrollo de destrezas para el aprendizaje', credits:2, sem:1, prereqs:[] },
+  { id:'LB1',  name:'Laboratorio de Biología I', credits:1, sem:1, prereqs:[] },
+  { id:'EX1',  name:'Extra Académica/Cultural o Deportiva', credits:1, sem:1, prereqs:[] },
 
-const total = {creditos: 0, sumaNotas: 0, contNotas: 0};
+  { id:'SOC',  name:'Sociología de la Salud', credits:2, sem:2, prereqs:[] },
+  { id:'QO',   name:'Química orgánica', credits:3, sem:2, prereqs:['QG'] },
+  { id:'EG',   name:'Estadística general', credits:3, sem:2, prereqs:[] },
+  { id:'II',   name:'Inglés Instrumental', credits:3, sem:2, prereqs:[] },
+  { id:'BIO2', name:'Biología II', credits:3, sem:2, prereqs:['BIO1'] },
+  { id:'LB2',  name:'Laboratorio de Biología II', credits:1, sem:2, prereqs:['LB1'] },
+  { id:'FM',   name:'Física médica', credits:3, sem:2, prereqs:['MAT1'] },
 
-function construirMalla() {
-  const cont = document.getElementById('malla');
-  for (let sem =1; sem<=13; sem++) {
-    const caja = document.createElement('div');
-    caja.className='semestre';
-    caja.innerHTML = `<h2>${sem}${sem===13?' (Internado Rotatorio)':''} Semestre</h2>`;
-    Object.entries(ramos)
-      .filter(([n,v]) => v.sem===sem)
-      .forEach(([n,v]) => {
-        const r = document.createElement('div');
-        r.className='ramo bloqueado';
-        r.innerHTML=`${n} (${v.cr}cr)`;
-        r.onclick =()=> toggleAprobado(r,n);
-        caja.appendChild(r);
+  { id:'AN1',  name:'Anatomía I', credits:7, sem:3, prereqs:['BIO2','LB2'] },
+  { id:'EMB',  name:'Embriología', credits:3, sem:3, prereqs:['BIO2','LB2'] },
+  { id:'CS',   name:'Ciencias Sociales', credits:2, sem:3, prereqs:['SOC'] },
+  { id:'IT1',  name:'ITPP I', credits:1, sem:3, prereqs:['SOC'] },
+
+  { id:'BIOQ', name:'Bioquímica', credits:6, sem:4, prereqs:[] },
+  { id:'AN2',  name:'Anatomía II', credits:3, sem:4, prereqs:['AN1','EMB'] },
+  { id:'PE',   name:'Psicología evolutiva', credits:5, sem:4, prereqs:['EMB'] },
+  { id:'INF',  name:'Informática', credits:1, sem:4, prereqs:[] },
+  { id:'IT2',  name:'ITPP II', credits:1, sem:4, prereqs:['AN1','IT1'] },
+
+  { id:'FIS',  name:'Fisiología', credits:8, sem:5, prereqs:['BIOQ','AN2'] },
+  { id:'HIS',  name:'Histología', credits:4, sem:5, prereqs:['AN2'] },
+  { id:'ES',   name:'Estadística', credits:2, sem:5, prereqs:['INF','CS'] },
+  { id:'IT3',  name:'ITPP III', credits:1, sem:5, prereqs:['IT2'] },
+  { id:'EL5',  name:'Electiva', credits:1, sem:5, prereqs:[] },
+
+  { id:'MIC',  name:'Microbiología e inmunología clínica', credits:5, sem:6, prereqs:['FIS','HIS'] },
+  { id:'PAR',  name:'Parasitología', credits:4, sem:6, prereqs:['FIS','HIS'] },
+  { id:'EGA',  name:'Epidemiología gen. y saneam. ambiental', credits:3, sem:6, prereqs:['ES'] },
+  { id:'PM',   name:'Psicología médica', credits:6, sem:6, prereqs:['PE'] },
+  { id:'IT4',  name:'ITPP IV', credits:1, sem:6, prereqs:['IT3'] },
+  { id:'EL6',  name:'Electiva', credits:1, sem:6, prereqs:[] },
+
+  { id:'M1',   name:'Medicina I', credits:6, sem:7, prereqs:['MIC','PAR'] },
+  { id:'FIOP', name:'Fisiopatología', credits:4, sem:7, prereqs:['FIS'] },
+  { id:'ANP',  name:'Anatomía patológica', credits:6, sem:7, prereqs:['MIC','PAR'] },
+
+  { id:'C1',   name:'Cirugía I', credits:6, sem:8, prereqs:['M1','ANP'] },
+  { id:'GO1',  name:'Ginecología y Obstetricia I', credits:3, sem:8, prereqs:['M1','ANP'] },
+  { id:'PU',   name:'Puericultura', credits:2, sem:8, prereqs:['PM'] },
+  { id:'F1',   name:'Farmacología I', credits:4, sem:8, prereqs:['FIOP'] },
+  { id:'GEN2', name:'Genética', credits:1, sem:8, prereqs:['M1'] },
+
+  { id:'P1',   name:'Pediatría I', credits:6, sem:9, prereqs:['PU'] },
+  { id:'M2',   name:'Medicina II', credits:5, sem:9, prereqs:['M1','FIOP'] },
+  { id:'F2',   name:'Farmacología II', credits:2, sem:9, prereqs:['F1'] },
+  { id:'PS2',  name:'Psicopatología', credits:2, sem:9, prereqs:[] },
+  { id:'EL9',  name:'Electiva', credits:1, sem:9, prereqs:[] },
+
+  { id:'M3',   name:'Medicina III', credits:4, sem:10, prereqs:['M2','F2'] },
+  { id:'C2',   name:'Cirugía II', credits:3, sem:10, prereqs:['C1'] },
+  { id:'GO2',  name:'Ginecología y Obstetricia II', credits:6, sem:10, prereqs:['GO1'] },
+  { id:'DE',   name:'Deontología médica', credits:1, sem:10, prereqs:[] },
+  { id:'ES2',  name:'Epidemiología especial', credits:2, sem:10, prereqs:['EGA'] },
+
+  { id:'M4',   name:'Medicina IV', credits:5, sem:11, prereqs:['M3'] },
+  { id:'P2',   name:'Pediatría II', credits:6, sem:11, prereqs:['P1'] },
+  { id:'IMG',  name:'Imagenología', credits:1, sem:11, prereqs:['M2'] },
+  { id:'AD',   name:'Administración médica', credits:2, sem:11, prereqs:['DE','ES2'] },
+  { id:'ML',   name:'Medicina legal', credits:1, sem:11, prereqs:['C2','GO2'] },
+
+  { id:'PSI2', name:'Psiquiatría clínica', credits:5, sem:12, prereqs:['PS2'] },
+  { id:'M5',   name:'Medicina V', credits:6, sem:12, prereqs:['M4'] },
+  { id:'C3',   name:'Cirugía III', credits:3, sem:12, prereqs:['C2','IMG'] },
+  { id:'MT',   name:'Medicina del trabajo', credits:1, sem:12, prereqs:['AD'] },
+  { id:'HM',   name:'Historia de la medicina', credits:1, sem:12, prereqs:[] },
+
+  { id:'M6',   name:'Medicina VI', credits:4, sem:13, prereqs:['M5'] },
+  { id:'P3',   name:'Pediatría III', credits:4, sem:13, prereqs:['P2'] },
+  { id:'HY',   name:'Higiene mental y psicoterapia', credits:1, sem:13, prereqs:['PSI2'] },
+  { id:'TG',   name:'Trabajo de grado', credits:4, sem:13, prereqs:[] },
+  { id:'C4',   name:'Cirugía IV', credits:4, sem:13, prereqs:['C3'] },
+  { id:'GO3',  name:'Ginecología y Obstetricia III', credits:4, sem:13, prereqs:['GO2'] },
+  { id:'PAS',  name:'Pasantía rural', credits:4, sem:13, prereqs:['MT'] }
+];
+
+let approved     = new Set();
+let totalCredits = 0;
+let gradeMap     = {};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const container = document.getElementById('container');
+  const creditsEl = document.getElementById('credits');
+  const averageEl = document.getElementById('average');
+  const maxSem    = Math.max(...courses.map(c => c.sem));
+
+  // Render de semestres y cursos
+  for (let s = 1; s <= maxSem; s++) {
+    const row = document.createElement('div');
+    row.className = 'semester-row';
+
+    courses.filter(c => c.sem === s).forEach(c => {
+      const box = document.createElement('div');
+      box.className = 'course' + (c.prereqs.length ? ' locked' : '');
+      box.id        = c.id;
+      box.innerHTML = `
+        <div class="title">${c.name}</div>
+        <div class="credit">${c.credits} cr</div>
+        <div class="grade-container">
+          <input type="number" min="0" max="100" placeholder="Nota">
+        </div>
+      `;
+
+      // Click para aprobar
+      box.addEventListener('click', () => {
+        if (box.classList.contains('locked') || box.classList.contains('approved'))
+          return;
+        box.classList.add('approved');
+        box.querySelector('.grade-container').style.display = 'block';
+        approved.add(c.id);
+        totalCredits += c.credits;
+        creditsEl.innerText = `Créditos: ${totalCredits}`;
+        unlockCourses();
       });
-    cont.appendChild(caja);
+
+      // Input de nota
+      const input = box.querySelector('input');
+      input.addEventListener('input', () => {
+        const v = parseFloat(input.value);
+        if (!isNaN(v)) gradeMap[c.id] = v;
+        else delete gradeMap[c.id];
+        updateAverage();
+      });
+
+      row.appendChild(box);
+    });
+
+    container.appendChild(row);
   }
-}
 
-function toggleAprobado(el, nombre) {
-  const datos = ramos[nombre];
-  if (el.classList.contains('bloqueado')) return;
-  if (el.classList.contains('aprobado')) return;
-  // Aprobar
-  el.classList.add('aprobado');
-  const inputNota = document.createElement('input');
-  inputNota.className='nota';
-  inputNota.type='number'; inputNota.min=0; inputNota.max=7; inputNota.step=0.1;
-  inputNota.placeholder='nota';
-  inputNota.onchange = e => { actualizaNota(nombre, parseFloat(e.target.value)); };
-  el.appendChild(inputNota);
-  // sumar créditos
-  total.creditos+= datos.cr;
-  document.getElementById('totalCreditos').innerText = total.creditos;
-  // desbloquear dependientes
-  if (datos.opens) datos.opens.forEach(dep=> desbloquear(dep));
-}
+  // Desbloquea los cursos del 1er semestre (smart)
+  unlockCourses();
 
-function desbloquear(nombre) {
-  document.querySelectorAll('.ramo').forEach(el=>{
-    if (el.textContent.startsWith(nombre)) {
-      el.classList.remove('bloqueado');
+  // Funciones auxiliares
+  function unlockCourses() {
+    courses.forEach(c => {
+      if (!approved.has(c.id) && c.prereqs.length) {
+        const ok = c.prereqs.every(pid => approved.has(pid));
+        if (ok) document.getElementById(c.id).classList.remove('locked');
+      }
+    });
+  }
+
+  function updateAverage() {
+    const notas = Object.values(gradeMap);
+    if (!notas.length) {
+      averageEl.innerText = 'Promedio: 0.00';
+      return;
     }
-  });
-}
-
-function actualizaNota(nombre, nota) {
-  if (isNaN(nota)) return;
-  total.sumaNotas += nota;
-  total.contNotas++;
-  document.getElementById('promedioNotas').innerText = (total.sumaNotas/total.contNotas).toFixed(2);
-}
-
-// inicializar
-window.onload = ()=>{
-  construirMalla();
-  // desbloquear ramos del primer semestre
-  Object.entries(ramos).forEach(([n,v])=>{
-    if (v.sem===1 ) desbloquear(n);
-  });
-};
+    const suma = notas.reduce((a,b) => a+b, 0);
+    averageEl.innerText = `Promedio: ${(suma/notas.length).toFixed(2)}`;
+  }
+});
